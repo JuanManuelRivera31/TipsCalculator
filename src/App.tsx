c6f2867b666a14370aa6b071a287a73950db6a15
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-02-24 17:41:44 -0500

    Inicializando el proyecto ysus herramientas

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { menuItems } from "./data/data"
+
+function App() {
+
+  return (
+    <>
+    <header className="bg-teal-400 py-5">
+      <h1 className="text-center text-4xl font-black
+      ">Calculadora de propinas y consumo</h1>
+    </header>
+
+    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+      <div>
+          <h2>Menú</h2>
+      </div>
+      
+      <div>
+          <h2>Consumo</h2>
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
